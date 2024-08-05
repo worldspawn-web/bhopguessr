4bbb0cef5bf27340b43b837493af0b03d4f08ecb
Michael Lozitckii <worldspawn.web@gmail.com>
2024-08-05 22:16:49 +0300

    refactor(tests): added baseURL instead of hardcode

--- frontend/tests/startup.spec.ts
+++ frontend/tests/startup.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
 test("Loaded successfully!", async ({ page }) => {
-  await page.goto("http://localhost:5173");
+  await page.goto("/");
   await expect(page).toHaveTitle(/Guessr/);
 });
