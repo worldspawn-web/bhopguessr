6d095aede5985b4e4800f652f84ca499a5cc6aae
Michael Lozitckii <worldspawn.web@gmail.com>
2024-08-13 00:44:19 +0300

    draft(tests): fix of fix no.5

--- frontend/tests/startup.spec.ts
+++ frontend/tests/startup.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test";
 
-test("Loaded successfully!", async ({ page }) => {
+test("Loaded successfully!", async ({ page, baseURL }) => {
+  console.log(baseURL);
   await page.goto("/");
   await expect(page).toHaveTitle(/Guessr/);
 });
